f050de73c029b35e3e75e21049ea9dd655a5f63c
PURBITA <2022mt93326@wilp.bits-pilani.ac.in>
2022-11-02 01:12:08 +0530

    added API to find employee by id

--- ems/src/main/java/com/bits/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/bits/ems/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.bits.ems.controller;
 
 import com.bits.ems.dto.Employee;
+import com.bits.ems.exception.NotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -13,14 +15,24 @@ public class EmployeeController {
     private final List<Employee> employeeList = new ArrayList<>();
 
     public EmployeeController() {
-        employeeList.add(new Employee(UUID.randomUUID(), "Purbita Jana","9088456700"));
-        employeeList.add(new Employee(UUID.randomUUID(), "Priya Prakash","9900989099"));
-        employeeList.add(new Employee(UUID.randomUUID(), "Suneel Patil","7899870098"));
+        employeeList.add(new Employee(UUID.randomUUID(), "Purbita Jana", "9088456700"));
+        employeeList.add(new Employee(UUID.randomUUID(), "Priya Prakash", "9900989099"));
+        employeeList.add(new Employee(UUID.randomUUID(), "Suneel Patil", "7899870098"));
     }
 
     @GetMapping
     public List<Employee> getEmployees() {
         return employeeList;
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable String id) {
+        Optional<Employee> employee = employeeList.stream().filter(emp -> emp.getId().equals(UUID.fromString(id))).findAny();
+        if (employee.isPresent()) {
+            return employee.get();
+        } else {
+            throw new NotFoundException("Employee not found");
+        }
     }
 
     @PostMapping
---
+++ ems/src/main/java/com/bits/ems/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.bits.ems.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
